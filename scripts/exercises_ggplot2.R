4bb78f5a46484d696d9ab0a87d08ee3e5bdde5ec
Paula Andrea Martinez <orchid00@gmail.com>
2018-03-02 12:49:17 +0100

    Have only questions

---
+++ scripts/exercises_ggplot2.R
@@ -0,0 +1,42 @@
+# Description: Introduction to tidyverse
+#              Elixir Belgium workshop
+#              Exercises ggplot2
+# Date: 
+# Author:
+# Note: - The exercises can have multiple solutions.
+#       - It is recommended to start new session for each script 
+#       (Session > New Session)
+
+# If you haven't installed tidyverse, please uncomment the following line
+# install.packages("tidyverse")
+
+# load the package tidyverse
+library(tidyverse)
+
+## 1. Read in sampledata.tsv
+
+
+## 2. Explore the dataset
+
+
+# Save each of following plot in the plots folder
+## 3. Plot the amount of males and females in this study using a bar plot
+
+
+## 4. Do the same but show the nationality and flip the axes 
+
+
+## 5. Create a boxplot showing the age distribution of each nationality. 
+## Use the fill or colour aesthetic to make it a little bit more colorful
+
+
+## 6. Add an extra layer to 5. with plotting points over the boxplot. 
+## Remove that layer again and explore the difference with geom_jitter()
+
+## 7. Make a density plot of the age distribution coloured by gender, 
+## faceted by nationality.
+## Extra: try one plot per row, and use a transparency of 0.5, 
+## you can also add geom_rug()
+
+
+# Well done! 
---
+++ scripts/exercises_tidyr.R
@@ -0,0 +1,47 @@
+# Description: Introduction to tidyverse
+#              Elixir Belgium workshop
+#              Exercises tidyr
+# Date: 
+# Author:
+# Note: - The exercises can have multiple solutions.
+#       - It is recommended to start new session for each script 
+#       (Session > New Session)
+
+# If you haven't installed tidyverse, please uncomment the following line
+# install.packages("tidyverse")
+
+# load the package tidyverse
+library(tidyverse)
+
+## 1. Import the file otutable.tsv as a tibble. 
+
+
+## 2. Tidy the tibble. 
+## Hint: results in a tibble with three columns: taxon, sample, abundance. 
+## resulting dimensions: 152360x3
+
+
+## 3. Visualise the distribution of abundance using a density plot.
+
+
+## 4. Add a fourth column with relative abundances within a sample. 
+## Call it “rel_ab_sample. 
+## Hint: abundance divided by the sum of abundances per sample.
+
+
+## 5. Filter the tibble so that only taxa with a mean relative abundance of at 
+## least 1% are retained.
+## Important: this is not the same as filtering out rows with rel_abundance < 1%.
+## Hint: first try to make a column “mean_taxon_rel_ab”. 
+## Resulting dimensions: 30472x5
+
+
+## 6. Visualise the distribution of mean_taxon_rel_ab using a density plot.
+
+
+## 7. Make a tile plot to visualise mean_taxon_rel_ab. 
+## Put the sample on the x-axis and taxon on the y-axis. 
+## Extra: Which taxa is the one with higher value of mean_taxon_rel_ab?
+
+
+# Great job!
