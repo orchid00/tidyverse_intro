7ddb23ee725e309c084081aebc132abaff82b0d8
Paula Andrea Martinez <orchid00@gmail.com>
2018-03-12 16:16:08 +0100

    new demo ggplot2

--- examples/demo_ggplot2.R
+++ examples/demo_ggplot2.R
@@ -21,32 +21,23 @@ plants <- read_tsv("data/plants.tsv")
 glimpse(plants)
 
 ## First plot
-ggplot(data = plants) +
-  geom_bar(mapping = aes(x = status, fill = status))
-
-# it is the same as
-ggplot(data = plants, mapping = aes(x = status, fill = status)) +
-  geom_bar()
+ggplot(data = plants, mapping = aes(x = status, y = size_mb)) +
+  geom_boxplot() +
+  scale_y_log10()
 
 ## Second plot
-ggplot(plants, aes(x = status, y = size_mb)) +
-  geom_boxplot()
+ggplot(plants, aes(x = genes, y = gc)) +
+  geom_point() +
+  geom_smooth(colour = "gold")
 
 ## Third plot
-ggplot(plants, aes(x = gc, y = size_mb)) +
-  geom_point()
-
-## Third plot + additional aesthetic
-ggplot(plants, aes(x = gc, y = size_mb, color = subgroup)) +
-  geom_point()
-
-ggplot(plants, aes(x = gc, y = size_mb, color = subgroup)) +
-  geom_point(alpha = 0.3)
+ggplot(plants, aes(x = size_mb, fill = subgroup , colour = subgroup)) +
+  geom_histogram(alpha = 0.3, bins = 50)
 
 ## Third plot + additional facet layer
-ggplot(plants, aes(x = gc, y = size_mb, color = subgroup)) +
-  geom_point() +
-  facet_wrap(~subgroup)
+ggplot(plants, aes(x = size_mb, fill = subgroup , colour = subgroup)) +
+  geom_histogram(alpha = 0.3, bins = 50) +
+  facet_wrap(~ subgroup)
 # Save last plot
 ggsave("plots/scatterplot_GC_by_size.png")
 
